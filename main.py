5be68c9339f9658d9830f91696b29a88bbe6ea1a
WumpusTheCoder <61352590+WumpusTheCoder@users.noreply.github.com>
2020-02-23 10:11:51 +0000

    Done Main.py.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
 import random
 import math, sys, time
-
 import ctx as ctx
 from discord import *
 from decimal import *
 from discord.ext import commands
 
-token = 'NjgwMTQ5ODQ5NTA4OTM3Nzgw.XlEnQQ.kAhDQ9AjQDbNtwD4rzzBHRdlzGc'
+token = 'Your Bot Token here in Speechmarks'
 bot = commands.Bot(command_prefix='$')
 
 print("Bot Online and Connected")
@@ -23,31 +22,14 @@ async def repeat(ctx, message, n: int):
     await ctx.send(f"Completed {n} times.")
 
 
-@bot.command(name='kill', help='Tries to stop ANY commands running any further.')
-async def kill(ctx):
-    await ctx.send("Trying to stop now.")
-    killswitch = True
-
 @bot.command(name='nuke', help='BREAKS DISCORD!')
-async def nuke(ctx, killswitch=False):
+async def nuke(ctx):
     n: int
     n = 100000000000000000000000000000000000000000000000000000000
-    if killswitch == False:
-        if n <= 100000000000000000000000000000000000000000000000000000000:
-            for i in range(n):
-                await ctx.send("nuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke ")
-        await ctx.send(f"If you see this, u survived somehow.")
-
-
-@bot.command(name='test', help='A special command for testing made by Eli3Bear.')
-async def test(ctx):
-    await ctx.send("Trying to stop now.")
-    await ctx.send("Trying to stop now.")
-    await ctx.send("Trying to stop now.")
-    await ctx.send("Trying to stop now.")
-    await ctx.send("Trying to stop now.")
-    await ctx.send("Trying to stop now.")
-    await ctx.send("Trying to stop now.")
+       if n <= 100000000000000000000000000000000000000000000000000000000:
+           for i in range(n):
+               await ctx.send("nuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke \nnuke ")
+       await ctx.send(f"If you see this, u survived somehow.")
 
 
 bot.run(token)
